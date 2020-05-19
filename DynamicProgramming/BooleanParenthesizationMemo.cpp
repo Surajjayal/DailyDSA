740620b94702ab8f93ee4d547be259de11bbe3a9
Surajjayal <46057297+Surajjayal@users.noreply.github.com>
2020-05-20 01:48:25 +0530

    Update BooleanParenthesizationMemo.cpp

--- DynamicProgramming/BooleanParenthesizationMemo.cpp
+++ DynamicProgramming/BooleanParenthesizationMemo.cpp
@@ -1,109 +1,73 @@
 /*recursive + memorization = top down approach O(n^2) approax*/
+#include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
-#define fast ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
-
+//unordered_map<string,int>m;
+//not working with map showing time limit exceed
 int dp[202][202][2];
-
-
-int BP(string a,int i,int j,int tb){
-
-  if(dp[i][j][tb] != -1)
-    return dp[i][j][tb];
-
-  //baseCase
-  if(i>j)//no chars
-    return dp[i][j][tb] = 0;
-
-  if( i == j){//if only one element
-    if(tb  == 1)//what we need is true value
-      return dp[i][j][tb] = (a[i] == 'T')? 1 : 0;
-    else return dp[i][j][tb] = (a[i] == 'F')? 1 : 0;
-  }
-
-
-  //partionFinding
-  int ans = 0;
-  for(int k=i+1;k<j;k+=2){
-
-    int lt ;//if the left side is true
-    int lf ;//if the left side is false
-    int rt ;//if the right side is true
-    int rf ;//if the right side is fals
-
-
-    if(dp[i][k-1][1]!=-1)
-            lt=dp[i][k-1][1];
+int parenthesization(string &s,int i,int j,bool istrue){
+    if(i>j)
+        return 0;
+    if(i==j){
+          if(istrue==true){
+            return s[i]=='T';
+        }  
         else
-        {
-            lt=BP(a,i,k-1,1);
-            dp[i][k-1][1]=lt;
-        }
-        if(dp[i][k-1][0]!=-1)
-            lf=dp[i][k-1][0];
-        else
-        {
-            lf=BP(a,i,k-1,0);
-            dp[i][k-1][0]=lf;
-        }
-        if(dp[k+1][j][1]!=-1)
-            rt=dp[k+1][j][1];
-        else
-        {
-            rt=BP(a,k+1,j,1);
-            dp[k+1][j][1]=rt;
-        }
-        if(dp[k+1][j][0]!=-1)
-            rf=dp[k+1][j][0];
-        else
-        {
-            rf=BP(a,k+1,j,0);
-            dp[k+1][j][0]=rf;
-        }
-
-    if(a[k] == '|'){
-      if(tb == 1)
-        ans += lt * rt + lf * rt + lt * rf; //counting total ways if we need true
-      else ans += lf * rf ;//if we need false
+            return s[i]=='F';
     }
-
-    if(a[k] == '&'){
-      if(tb == 1)
-        ans += lt * rt;
-      else ans += lf * rf + lt * rf + lf * rt;
+    // string temp=to_string(i);
+    // //temp+= ' ';
+    // temp=temp.append(to_string(j));
+    // //temp+= ' ';
+    // temp=temp.append(to_string(istrue));
+    // if(m.find(temp)!=m.end())
+    //   return m[temp];
+      
+    if(dp[i][j][istrue]!=-1)
+     return dp[i][j][istrue];
+    int ans=0;
+   // cout<<"temp="<<temp<<" ";
+    for(int k=i+1;k<j;k+=2){
+        int lt=parenthesization(s,i,k-1,true);
+        int lf=parenthesization(s,i,k-1,false);
+        int rt=parenthesization(s,k+1,j,true);
+        int rf=parenthesization(s,k+1,j,false);
+        if(s[k]=='|'){
+            if(istrue==true)
+              ans=ans + lt*rt + lt*rf +lf*rt;  
+            else
+                ans=ans + lf*rf;
+        }
+        else if(s[k]=='&'){
+              if(istrue==true)
+              ans=ans + lt*rt ;  
+            else
+                ans=ans + lt*rf +lf*rt + lf*rf;
+        }
+        else if(s[k]=='^'){
+            if(istrue==true)
+              ans=ans + lf*rt + lt*rf ;  
+            else
+                ans=ans+ lt*rt + lf*rf;
+        }
     }
-
-    if(a[k] == '^'){
-      if(tb == 1)
-        ans += lf * rt + lt * rf ;
-      else ans += lt * rt + lf * rf;
-    }
-
-  }
-
-  return dp[i][j][tb] = ans % 1003;
-
+    ans=ans % 1003;
+   // m[temp]=ans;
+    dp[i][j][istrue]=ans;
+    return ans;
 }
-
-
-int main(){
-  fast;
-  int t;
-  cin>>t;
-
-  while(t--){
-
-    int n;
-    cin>>n;
-
-    string a;
-    cin>>a;
-
-    memset(dp,-1,sizeof(dp));
-
-    cout<<BP(a,0,n-1,1)<<endl;
-
-  }
-
-  return 0;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    string s;
+	    cin>>s;
+	   //m.clear();
+	   memset(dp,-1,sizeof(dp));
+	    int ans= parenthesization(s,0,n-1,true);
+	    cout<<ans<<endl;
+	}
+	return 0;
 }
